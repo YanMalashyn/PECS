7878ae829bc098bee3713e79548e711acbe15767
Yan.Malashyn <lamantinspb1@gmail.com>
2022-04-08 20:32:13 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,19 @@ public class Main {
 
     //Producer можно достать сам класс и всех родителей.
     public static void someMethod (List<? extends Class3> list) {
+        //не сработает
         Class4 class4 = list.get(0);
+        //все ок
         Class2 class2 = list.get(1);
         //можно положить null
         list.add(null);
     }
     //Consumer можно положить сам класс и наследников
     public static void someMethod2 (List<? super Class3> list) {
-        list.add(new Class4());
+        //не сработает
         list.add(new Class2());
+        //все ок
+        list.add(new Class4());
         //вернет Object
         Object object = list.get(0);
     }
